a5de8b232fc578e94070144d509e7d8b7b595a2f
meghagoel <meghagoel211@gmail.com>
2023-01-19 15:59:03 +0530

    Adding Base code

---
+++ cars-studio/src/App.js
@@ -0,0 +1,15 @@
+import CarList from "./components/CarList";
+import CarSearch from "./components/CarSearch";
+import CarValue from "./components/CarValue";
+import CarForm from "./components/CarForm";
+
+function App() {
+    return <div className="container is-fluid">
+        <CarForm />
+        <CarSearch />
+        <CarList />
+        <CarValue/> 
+    </div>;
+}
+
+export default App;
